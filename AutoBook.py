0f4cb6b7a801593016a97f39d0bf373790c03fe1
Kevin McCabe <kmccabe2@cisco.com>
2013-10-22 08:35:23 -0400

    McCabe Commit, updated imports, less mouse clicks, wait for print screen to load, no mouse calibration yet

--- AutoBook.py
+++ AutoBook.py
@@ -1,36 +1,66 @@
-from appscript import app
+#imports
 from pymouse import PyMouse
+from pykeyboard import PyKeyboard
 import time
+import os
 
-#print location: (358.69140625, 282.88671875)
-#hit enter: app('System Events').key_code(76)
-# click PDF: (569.25390625, 337.71484375)
-# press s
-# press enter
-# pass "page" + x
-# pass enter
-#newpage = n
-# sleep between each thing at least .01 seconds time.sleep(seconds)
-
+#init objects
 m = PyMouse()
+k = PyKeyboard()
 
-if True:
-	numPages = 391 #pages in doc
-	i = 197
-	m.click(358.69140625, 282.88671875, 1) #activate sarfari
+#global vars, need to check before EVERY RUN
+finalFileNm = "WholeBook.pdf"
+startDir = "/home/kdm/"
+finalFolder = "/home/kdm/RippedBooks"
+numPages = 348 #pages in Doc
+i = 1 #starting page number
+
+def createPDFs():
+	#while loop loops through all pages and creates a pdf, 
+	#also moves those pdfs into a dedicated folder
+	global i, m, k
 	while i <= numPages:
-		m.click(358.69140625, 282.88671875, 1) #click print
-		time.sleep(.25) #wait
-		app('System Events').key_code(76) #press enter
-		time.sleep(.5) #wait
-		m.click(570.97265625, 338.7890625, 1) #click PDF
-		time.sleep(.5) #wait
-		m.click(600.03125, 377.8515625, 1) #click PDF
-		time.sleep(.25) #wait
-		app('System Events').keystroke('page'+ "0" * (3 - len(str(i))) + str(i)) #type pdf name
-		time.sleep(0.25) #wait
-		app('System Events').key_code(76) #press enter
-		time.sleep(1.25) #wait
-		app('System Events').keystroke('n') #press n for next page
-		time.sleep(4) #wait
-		i += 1
+		m.click(421, 288) #click print
+		#wait until the print window is open
+		while str(os.system("wmctrl -l | grep 'kdm-virtual-machine Print'")) == "256":
+			time.sleep(2)
+			print "waiting"
+		k.tap_key(k.tab_key) #select printer
+		time.sleep(2)
+		k.tap_key(k.tab_key) #enter into filename field
+		time.sleep(.25)
+		filenm = 'page' + str(i).zfill(5) + ".pdf"
+		k.type_string(filenm) #enter filename
+		time.sleep(.25)
+		k.tap_key(k.enter_key) #press enter to print
+		time.sleep(5)
+		k.press_key(k.alt_key) #press alt+F4 to close the window
+		k.tap_key(k.function_keys[4])
+		k.release_key(k.alt_key)
+		time.sleep(1)
+		m.click(497, 385) #ensure focus is back in page
+		if not os.path.isfile(startDir + filenm):
+			print "File was NOT created, stopping loop"
+			return False
+			break
+		#move file to other folder
+		os.system("mv " + startDir + filenm + " " + finalFolder) #move file to desired folder
+		time.sleep(.25)
+		k.type_string('n') #press n for next page
+		time.sleep(3)
+		i +=1
+	return True
+
+def mergePDfs():
+	#create one merged PDF of all files
+	pdfCatCmd = "gs -dBATCH -dNOPAUSE -dSAFER -q -sDEVICE=pdfwrite -sOutputFile=" + finalFolder + "/" + \
+		finalFileNm + " " + finalFolder + "/*.pdf "
+	os.system(pdfCatCmd)
+
+if __name__ == '__main__':
+	print "Start Time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+	if createPDFs():
+		mergePDfs()
+	else:
+		print "a problem occurred"
+	print "End Time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
