461b6c01674b4bc6b65a69f3b46b70be3f6f5f7c
Kevin McCabe <kmccabe2@cisco.com>
2013-10-24 19:25:39 -0400

    Almost all basic gui functions done, see to-do in createGUI.py for more info

--- CreateGUI.py
+++ CreateGUI.py
@@ -1,4 +1,11 @@
-
+"""To do:
+	1. Save As button
+	2. Figure out why extra lines are going into saved files
+	3. Move Start Num / End Num values beneath action list
+	4. hide backend listbox
+	5. maybe look at moving arrow buttons over to right side of list
+	6. implement grid painting instead of pack
+"""
 #imports
 from Tkinter import *
 from pykeyboard import PyKeyboard
@@ -14,12 +21,14 @@ readyForMouseInput = False
 specialkeys= {"Ctrl" : "control_l_key", "Alt" : "alt_l_key", "Del" : "delete_key", "Insert":"insert_key","Esc":"escape_key","Special Keys":""}
 headerLst = ["Order","Action Type", "Value", "Comment"]
 delimChars = "<~>"
+loadedFileName = ""
+beenSaved = True
+#note in createWidgets(), we create globals as well
 
 #functions
 def recMouseClick():
     #Fill text box with string "press m to grab mouse location"
     #set global var to something to signify that we are ready to use event when we hit m
-    global tRecMouse
     tRecMouse.delete(0, END)
     sendMessage("Press m to grab mouse location, or enter in textbox")
     master.bind("m",getMouseInput)
@@ -61,23 +70,39 @@ def specialKeyInsert():
 
 def saveAction():
 	#saves the current action to the listbox
+	global beenSaved
 	#ensure only one textbox is filled out
 	total = int(tRecMouse.get().strip() != "")
 	total += int(tEnterString.get().strip() != "")
 	total += int(tWait.get().strip() != "")
 	total += int(tWaitScreen.get().strip() != "")
 	
-	print tRecMouse.get().strip()
-	
-	#false is zero
-	#true is one
-	
-	print total
-	
 	if total == 1:
 		#exactly 1 input
 		#save action to masterListbox then refresh listbox
-		orderNum = lb
+		orderNum = lbActionsBackend.size() + 1
+		
+		#get action specific values
+		if tRecMouse.get().strip() != "":
+			actionType = "Click Mouse"
+			actionVal = tRecMouse.get()
+			
+		elif tEnterString.get().strip() != "":
+			actionType = "Pass Keys"
+			actionVal = tEnterString.get()
+			
+		elif tWait.get().strip() != "":
+			actionType = "Wait Seconds"
+			actionVal = tWait.get()
+			
+		elif tWaitScreen.get().strip() != "":
+			actionType = "Wait for Screen"
+			actionVal = tWaitScreen.get()
+
+		Comment = tComment.get()
+		addLBItem([orderNum, actionType, actionVal, Comment])
+		beenSaved = False
+		clearInputs()
 		
 	elif total == 0:
 		#no inputs
@@ -86,207 +111,306 @@ def saveAction():
 		#more than one input
 		tkMessageBox.showwarning("Wrong number of inputs!", "Entering more than one action at a time is not allowed, please only select a Mouse, Keyboard, Wait or Window action")
 		
-		
 def delAction():
 	#deletes selected action
+	global beenSaved
 	if len(lbActions.curselection()) == 0 or int(lbActions.curselection()[0]) == 0:
 		pass
 		#We will not allow you to delete the header, also if nothing is selected, do nothing
 	else:
-		lbActionsBackend.delete(lbActions.curselection())
+		lbActionsBackend.delete(str(int(lbActions.curselection()[0]) - 1)) #have to account for header row
 		repaintActionLb()
+		beenSaved = False
 
 def loadConfig():
 	#load a saved config
 	#ask user to choose file:
-	loadFile = tkFileDialog.askopenfile()
+	global beenSaved, loadedFileName
+	
+	#determine if user gets option to save, provide if necessary
+	if not(beenSaved):
+		#ask user if they want to save
+		if tkMessageBox.askyesno("Save?","Would you like to save your current list?"):
+			if len(loadedFile) > 0:
+				saveConfig()
+			else:
+				saveAsConfig()
+	
+	loadFile = tkFileDialog.askopenfile(filetypes=[("workflow files",".fecn")])
 	
 	i = 0
+	
+	if loadFile == None:
+		#no file returned
+		print "No File Returned"
+		return
+		
 	lines = loadFile.readlines()
-	#get each line in file
+	
+	#Clear existing file
 	lbActionsBackend.delete(0,END)
-	addLBItem(headerLst)
+		
 	for line in lines:
 		#loop through line for args
 		fileArgs = []
-		currLine = line.replace("\n","")
+		currLine = line#.strip("\n") <-need for saving
 		fileArgs = currLine.split(delimChars)
 		if int(fileArgs[0]) == -1:
-			#End num
+			#Populate End num textbox
 			tStartNum.delete(0,END)
 			tStartNum.insert(0,fileArgs[2])
 		elif int(fileArgs[0]) == -2:
-			#start num
+			#Populate start num textbox
 			tEndNum.delete(0,END)
 			tEndNum.insert(0,fileArgs[2])
 		else:
+			#Add row to lbActionsBackend
 			addLBItem(fileArgs)
 		i += 1
-		currline = str(loadFile.readline(1))
-	repaintActionLb()
-
+	loadedFileName = loadFile.name
+	loadFile.close()
+	beenSaved = True
+	
 def clearInputs():
-	pass
+	tRecMouse.delete(0, END)
+	tEnterString.delete(0, END)
+	tWait.delete(0, END)
+	tWaitScreen.delete(0, END)
+	sendMessage("")
+
+#clear screen and vars for new sequence
+def newActionSet():
+	#ask user if they want to save first
+	global beenSaved
+	
+	#determine if user needs option to save, provide if necessary
+	if not(beenSaved):
+		if tkMessageBox.askyesno("Save?","Would you like to save your current list?"):
+			if loadeFileName != "":
+				saveConfig()
+			else:
+				saveConfigAs()
+	
+	#clear everything and reinitialize screen
+	clearInputs()
+	tStartNum.delete(0, END)
+	tEndNum.delete(0, END)
+	lbActions.delete(0, END)
+	lbActionsBackend.delete(0, END)
+	lbActions.insert(END, "Order".ljust(8) + "|" + "Action Type".ljust(20) + \
+		"|" + "Value".ljust(25) + "|" + "Comment".ljust(25))
+	beenSaved = True	
 
 def saveConfig():
 	#save current config, if currently saved, overwrite, if not, pop up dialog box
+	#determine if we already have a file loaded, if so, proceed, if not, open dialog
+	#loop through lbActionsBackend to get data
+	#write data to file
+	global beenSaved
+	saveFile = tkFileDialog.asksaveasfile(mode='w',defaultextension=".fecn")
+	#ensure filetype is fecn
+	if saveFile.name[len(saveFile.name)-5:len(saveFile.name)] == ".fecn":
+		#file is good continue with save
+		#first need to save start num / end num
+		saveFile.write("-1<~>StartNum<~>%s<~>\n" % tStartNum.get())
+		saveFile.write("-2<~>EndNum<~>%s<~>\n" % tEndNum.get())
+		saveFile.writelines(lbActionsBackend.get(0,END))
+		saveFile.close()
+	else:
+		#wrong filename
+		pass
+		
+	beenSaved = True
 	pass
 
 def saveConfigAs():
 	#save as, always pop up dialog box
+	global beenSaved
+	beenSaved = True
 	pass
 
-def moveItem(moveVal):
-	#move selected item up/down
+def moveItemUp():
+	moveItem(-1)
+	
+def moveItemDwn():
+	moveItem(1)
+	
+def moveItem(moveDir):
+	#move selected item up/down, moveDir should be 1 (move down) or -1 (move up)
+	global beenSaved
+
+	#check that an item is selected
+	if len(lbActions.curselection()) > 0:
+		#get selected item index and value
+		lstindex = int(lbActions.curselection()[0])
+		curItm = lbActionsBackend.get((str(lstindex - 1)))
+		
+		#ensure movement is allowed
+		print lbActions.size()
+		if (lstindex + moveDir > 0) and (lstindex + moveDir < lbActions.size()) and \
+				(lstindex !=0):
+			#delete item
+			lbActionsBackend.delete(lstindex - 1)
+		
+			#reinsert item at proper position
+			lbActionsBackend.insert(lstindex - 1 + moveDir, curItm)
+	
+			repaintActionLb()
+			lbActions.activate(lstindex + moveDir)
+			lbActions.selection_set(lstindex + moveDir)
+			beenSaved = False
+		else:
+			print "not allowed"
 	pass
 
 #add an item to master listbox, repaint listbox user sees
 def addLBItem(argLst):
 	#order, action type, value, comment are column headers
-	lbActionsBackend.insert(END, argLst[0] + "<~>" + argLst[1] + \
-		"<~>" + argLst[2] + "<~>" + argLst[3])
+	lbActionsBackend.insert(END, str(argLst[0]) + "<~>" + str(argLst[1]) + \
+		"<~>" + str(argLst[2]) + "<~>" + str(argLst[3]) + "\n")
 	repaintActionLb()
 
 #Pull all data from master listbox and repaint what the user sees
 def repaintActionLb():
 	#Clear list and add header
-	lbActionsBackend.delete(0,END)
+	lbActions.delete(0,END)
 	lbActions.insert(END, "Order".ljust(8) + "|" + "Action Type".ljust(20) + \
 		"|" + "Value".ljust(25) + "|" + "Comment".ljust(25))
 	
 	i = 0
 	#loop through backend listbox and display values in user friendly way, will truncate chars if necessary
-	for i in range(lbActionsBackend.size()-1):
+	while i < lbActionsBackend.size():
 		lineStr = str(lbActionsBackend.get(i))
 		argLst = lineStr.split(delimChars)
 		lbActions.insert(END, argLst[0][0:8].ljust(8) + "|" + argLst[1][0:20].ljust(20) + \
 			"|" + argLst[2][0:25].ljust(25) + "|" + argLst[3][0:25].ljust(25))
+		i += 1
 
-#create GUI buttons
-lTitle = Label(master, text="Mouse / Keyboard Automation Tool",anchor = W, justify=LEFT)
-lTitle2 = Label(master, text="",anchor = W, justify=LEFT)
+def createWidgets():
+	#create GUI buttons
+	global lTitle, lTitle2, fMouse, lRecMouse, tRecMouse, bRecMouse, fEnterString, \
+		lEnterString, tEnterString, mEnterString, mEnterStringOptions, bEnterString, \
+		fStartNum, lStartNum, tStartNum, fEndNum, lEndNum, tEndNum, fWait, lWait, tWait, \
+		fWaitScreen, lWaitScreen, tWaitScreen, fComment, lComment, tComment, fSaveAction, \
+		bSaveAction, bDelAction, fMoveButtons, bUp, bDown, fActions, lActions, lbActions, \
+		lbActionsBackend, fSaves, bLoad, bSave, bSaveAs, fCanvas, lCanvas, cCanvas, bClear
+		
+	
+	lTitle = Label(master, text="Mouse / Keyboard Automation Tool",anchor = W, justify=LEFT)
+	lTitle2 = Label(master, text="",anchor = W, justify=LEFT)
 
-fMouse = Frame(master)
-lRecMouse = Label(fMouse, text="Click at Coordinates (x,y):", anchor = W, justify=LEFT, width=30)
-tRecMouse = Entry(fMouse, width = 8)
-bRecMouse = Button(fMouse, text="Get Coords", command=recMouseClick, width =8)
+	fMouse = Frame(master)
+	lRecMouse = Label(fMouse, text="Click at Coordinates (x,y):", anchor = W, justify=LEFT, width=30)
+	tRecMouse = Entry(fMouse, width = 8)
+	bRecMouse = Button(fMouse, text="Get Coords", command=recMouseClick, width =8)
 
-fEnterString = Frame(master)
-lEnterString = Label(fEnterString, text="Pass Keystrokes:", anchor = W, justify=LEFT, width=30)
-tEnterString = Entry(fEnterString)
-mEnterString = StringVar(fEnterString)
-mEnterString.set("Special Keys")
-##TO DO: Add support for F keys, Windows/Function/Mac key, Home/End, Page Up/Down
-mEnterStringOptions = OptionMenu(fEnterString, mEnterString, "Special Keys","Ctrl","Alt","Del","Insert","Esc")
-#mEnterStringOptions.bind("<ButtonRelease-1>",specialKeyInsert)
-bEnterString = Button(fEnterString, text="Add", command=specialKeyInsert, width=8)
+	fEnterString = Frame(master)
+	lEnterString = Label(fEnterString, text="Pass Keystrokes:", anchor = W, justify=LEFT, width=30)
+	tEnterString = Entry(fEnterString)
+	mEnterString = StringVar(fEnterString)
+	mEnterString.set("Special Keys")
+	##TO DO: Add support for F keys, Windows/Function/Mac key, Home/End, Page Up/Down
+	mEnterStringOptions = OptionMenu(fEnterString, mEnterString, "Special Keys","Ctrl","Alt","Del","Insert","Esc")
+	#mEnterStringOptions.bind("<ButtonRelease-1>",specialKeyInsert)
+	bEnterString = Button(fEnterString, text="Add", command=specialKeyInsert, width=8)
 
-fStartNum = Frame(master)
-lStartNum = Label(fStartNum, text="Starting Num:", anchor = W, justify=LEFT, width=30)
-tStartNum = Entry(fStartNum)
+	fStartNum = Frame(master)
+	lStartNum = Label(fStartNum, text="Starting Num:", anchor = W, justify=LEFT, width=30)
+	tStartNum = Entry(fStartNum)
 
-fEndNum = Frame(master)
-lEndNum = Label(fEndNum, text="Ending Num (inclusive):", anchor = W, justify=LEFT, width=30)
-tEndNum = Entry(fEndNum)
+	fEndNum = Frame(master)
+	lEndNum = Label(fEndNum, text="Ending Num (inclusive):", anchor = W, justify=LEFT, width=30)
+	tEndNum = Entry(fEndNum)
 
-fWait = Frame(master)
-lWait = Label(fWait, text="Wait Seconds:", anchor = W, justify=LEFT, width=30)
-tWait = Entry(fWait)
+	fWait = Frame(master)
+	lWait = Label(fWait, text="Wait Seconds:", anchor = W, justify=LEFT, width=30)
+	tWait = Entry(fWait)
 
-fWaitScreen = Frame(master)
-lWaitScreen = Label(fWaitScreen, text="Wait for window to open with name:", anchor = W, justify=LEFT, width=30)
-tWaitScreen = Entry(fWaitScreen)
+	fWaitScreen = Frame(master)
+	lWaitScreen = Label(fWaitScreen, text="Wait for window to open with name:", anchor = W, justify=LEFT, width=30)
+	tWaitScreen = Entry(fWaitScreen)
 
-fComment = Frame(master)
-lComment = Label(fComment, text="Enter a Comment with Action:", anchor = W, justify=LEFT, width=30)
-tComment = Entry(fComment)
+	fComment = Frame(master)
+	lComment = Label(fComment, text="Enter a Comment with Action:", anchor = W, justify=LEFT, width=30)
+	tComment = Entry(fComment)
 
-fSaveAction = Frame(master)
-bSaveAction = Button(fSaveAction, text="Save Action", command=saveAction, width=15)
-bDelAction = Button(fSaveAction, text="Delete Action", command=delAction, width=15)
+	fSaveAction = Frame(master)
+	bSaveAction = Button(fSaveAction, text="Save Action", command=saveAction, width=15)
+	bDelAction = Button(fSaveAction, text="Delete Action", command=delAction, width=15)
 
-fMoveButtons = Frame(master)
-bUp = Button(fMoveButtons, text=u"\u2191", command=moveItem(-1))
-bDown = Button(fMoveButtons, text=u"\u2193", command=moveItem(1))
+	fActions = Frame(master)
+	lActions = Label(fActions, text="Action List:", anchor = W, justify=LEFT, width=30)
+	lbActions = Listbox(fActions, width = 100, font=("Courier",12))
+	lbActionsBackend = Listbox(fActions, width=100)
+	repaintActionLb()
 
-fActions = Frame(master)
-lActions = Label(fActions, text="Action List:", anchor = W, justify=LEFT, width=30)
-lbActions = Listbox(fActions, width = 100, font=("Courier",12))
-lbActionsBackend = Listbox(fActions, width=1, height =1)
-repaintActionLb()
+	fMoveButtons = Frame(master)
+	bUp = Button(fMoveButtons, text=u"\u2191", command=moveItemUp)
+	bDown = Button(fMoveButtons, text=u"\u2193", command=moveItemDwn)
 
+	fSaves = Frame(master)
+	bLoad = Button(fSaves, text="Load Config", command=loadConfig)
+	bSave = Button(fSaves, text="Save Config", command=saveConfig)
+	bSaveAs = Button(fSaves, text="Save Config As...", command=saveConfigAs)
+	bClear = Button(fSaves, text="Clear Screen", command=newActionSet)
 
-fSaves = Frame(master)
-bLoad = Button(fSaves, text="Load Config", command=loadConfig)
-bSave = Button(fSaves, text="Save Config", command=saveConfig)
-bSaveAs = Button(fSaves, text="Save Config As...", command=saveConfigAs)
+	fCanvas = Frame(master)
+	lCanvas = Label(fCanvas, text="Messages:")
+	cCanvas = Canvas(fCanvas, height = 100, width = 400)
+	cCanvas.create_rectangle(5,5,390,100)
 
-fCanvas = Frame(master)
-lCanvas = Label(fCanvas, text="Messages:")
-cCanvas = Canvas(fCanvas, height = 100, width = 400)
-cCanvas.create_rectangle(5,5,390,100)
+def paintWidgets():
+	#paint everything
+	lTitle.pack()
+	lTitle2.pack()
 
-#paint everything
-lTitle.pack()
-lTitle2.pack()
+	fMouse.pack()
+	fEnterString.pack()
+	fStartNum.pack()
+	fEndNum.pack()
+	fWait.pack()
+	fWaitScreen.pack()
+	fComment.pack()
+	fSaveAction.pack()
+	fMoveButtons.pack()
+	fActions.pack()
+	fSaves.pack()
+	fCanvas.pack()
 
-"""
-fMouse.pack_propagate(0)
-fEnterString.pack_propagate(0)
-fStartNum.pack_propagate(0)
-fEndNum.pack_propagate(0)
-fWait.pack_propagate(0)
-fWaitScreen.pack_propagate(0)
-fComment.pack_propagate(0)
-fSaveAction.pack_propagate(0)
-fMoveButtons.pack_propagate(0)
-fActions.pack_propagate(0)
-fSaves.pack_propagate(0)
-fCanvas.pack_propagate(0)
-"""
+	lRecMouse.pack(side=LEFT)
+	tRecMouse.pack(side=LEFT)
+	bRecMouse.pack(side=LEFT)
+	lEnterString.pack(side=LEFT)
+	tEnterString.pack(side=LEFT)
+	mEnterStringOptions.pack(side=LEFT)
+	bEnterString.pack(side=LEFT)
+	lWait.pack(side=LEFT)
+	tWait.pack(side=LEFT)
+	lWaitScreen.pack(side=LEFT)
+	tWaitScreen.pack(side=LEFT)
+	tWait.pack(side=LEFT)
+	bSaveAction.pack(side=RIGHT)
+	bDelAction.pack(side=RIGHT)
+	bUp.pack(side=LEFT)
+	bDown.pack(side=LEFT)
+	lActions.pack()
+	lbActions.pack()
+	lbActionsBackend.pack()
 
-fMouse.pack()
-fEnterString.pack()
-fStartNum.pack()
-fEndNum.pack()
-fWait.pack()
-fWaitScreen.pack()
-fComment.pack()
-fSaveAction.pack()
-fMoveButtons.pack()
-fActions.pack()
-fSaves.pack()
-fCanvas.pack()
+	bLoad.pack(side=LEFT)
+	bSave.pack(side=LEFT)
+	bSaveAs.pack(side=LEFT)
+	bClear.pack(side=LEFT)
+	lCanvas.pack()
+	cCanvas.pack()
+	lComment.pack(side=LEFT)
+	tComment.pack(side=LEFT)
+	lStartNum.pack(side=LEFT)
+	tStartNum.pack(side=LEFT)
+	lEndNum.pack(side=LEFT)
+	tEndNum.pack(side=LEFT)
 
-lRecMouse.pack(side=LEFT)
-tRecMouse.pack(side=LEFT)
-bRecMouse.pack(side=LEFT)
-lEnterString.pack(side=LEFT)
-tEnterString.pack(side=LEFT)
-mEnterStringOptions.pack(side=LEFT)
-bEnterString.pack(side=LEFT)
-lWait.pack(side=LEFT)
-tWait.pack(side=LEFT)
-lWaitScreen.pack(side=LEFT)
-tWaitScreen.pack(side=LEFT)
-tWait.pack(side=LEFT)
-bSaveAction.pack(side=RIGHT)
-bDelAction.pack(side=RIGHT)
-bUp.pack(side=LEFT)
-bDown.pack(side=LEFT)
-lActions.pack()
-lbActions.pack()
-bLoad.pack(side=LEFT)
-bSave.pack(side=LEFT)
-bSaveAs.pack(side=LEFT)
-lCanvas.pack()
-cCanvas.pack()
-lComment.pack(side=LEFT)
-tComment.pack(side=LEFT)
-lStartNum.pack(side=LEFT)
-tStartNum.pack(side=LEFT)
-lEndNum.pack(side=LEFT)
-tEndNum.pack(side=LEFT)
-
+createWidgets()
+paintWidgets()
 
 master.mainloop()
